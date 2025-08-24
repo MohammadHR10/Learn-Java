4cabaf6717ae9a924166856d09a0bf0e1583febb
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-08-24 06:03:56 -0500

    new conditions

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int chocolates = 5;
+        while (chocolates > 0) {
+            System.out.println("I have " + chocolates + " chocolates");
+            chocolates--;
+        }
+        System.out.println("No more chocolates!");
+    }
+}
---
+++ src/dowhileloop.java
@@ -0,0 +1,10 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int study = 5;
+        do {
+            study += 1;
+            System.out.println("I have been studying for " + study + " hours");
+        } while (study <= 20);
+        System.out.println("I have studied");
+    }
+}
