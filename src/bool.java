92bc406474a111f052ed4baee12f02264db5e9a2
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-08-24 05:35:17 -0500

    New ones

---
+++ src/bool.java
@@ -0,0 +1,6 @@
+public class bool {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        System.out.println("Dipto is a student: " + isStudent);
+    }
+}
---
+++ src/charExample.java
@@ -0,0 +1,6 @@
+public class charExample {
+    public static void main(String[] args) {
+        char firstLetter = 'E';
+        System.out.println("The first letter of my name starts with " + firstLetter);
+    }
+}
