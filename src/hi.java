f6959afbb38f007ac5af194db64c189d3b7dd5a0
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-08-24 05:12:15 -0500

    Java program

---
+++ src/hi.java
@@ -0,0 +1,7 @@
+public class hi {
+    public static void main(String[] args) {
+        int age = 30;
+        int year = 2024;
+        System.out.println("Hello! I am " + age + " years old in the year " + year + ".");
+    }
+}
